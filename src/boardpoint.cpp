d9f7d790206fe5ec41598e36acac4119d8c01b70
tatwd <tatwdo@gmail.com>
2022-06-01 19:56:30 +0800

    fix `isDead`

---
+++ src/boardpoint.cpp
@@ -0,0 +1,6 @@
+#include "boardpoint.h"
+
+BoardPoint::BoardPoint()
+{
+
+}
---
+++ src/boardpoint.h
@@ -0,0 +1,11 @@
+#ifndef BOARDPOINT_H
+#define BOARDPOINT_H
+
+
+class BoardPoint
+{
+public:
+    BoardPoint();
+};
+
+#endif // BOARDPOINT_H
